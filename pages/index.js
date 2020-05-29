4eed941ae8e06e65419391bf1f9382ed5325f677
Mario Salinas <mario_1605@live.com.mx>
2020-05-29 06:17:03 -0500

    feature/ styles and images

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,18 @@ export default class extends React.Component {
       return <div>
         <h1>¡Hola Platzi!</h1>
         <p>Bienvenidos al curso de Next.JS</p>
+        <img src="/static/KTM.jpg" alt="motorcycle"></img>
+
+        <style jsx>{`
+            h1 {
+                color: red;
+            }
+            img {
+                max-width: 50%;
+                display: block;
+                margin: 0 auto;
+            }
+        `}</style>
       </div>
     }
   }
