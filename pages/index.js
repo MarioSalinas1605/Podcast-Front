043983166f56a34486fe39b99dacbc22252c5c10
Mario Salinas <mario_1605@live.com.mx>
2020-05-29 07:34:18 -0500

    feature/ recommended podcast from api with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,65 @@
 export default class extends React.Component {
-    render() {
-      return <div>
-        <h1>¡Hola Platzi!</h1>
-        <p>Bienvenidos al curso de Next.JS</p>
-        <img src="/static/KTM.jpg" alt="motorcycle"></img>
 
-        <style jsx>{`
-            h1 {
-                color: red;
+    static async getInitialProps(params) {
+        let req = await fetch('https://api.audioboom.com/channels/recommended')
+        let { body: channels } = await req.json()
+        return { channels }
+    }
+
+    render() {
+        const { channels } = this.props
+
+        return <div>
+            <header>Podcasts</header>
+            <div className="channels">
+                {
+                    channels.map((channel, index) => (
+                        <div className="channel" key={index}>
+                            <img src={channel.urls.logo_image.original} alt=""></img>
+                            <h2>{channel.title}</h2>
+                        </div>
+                    ))
+                }
+            </div>
+
+            <style jsx>{`
+            header {
+                color #fff;
+                background: #8756ca;
+                padding: 15px;
+                text-align: center;
             }
-            img {
-                max-width: 50%;
+            .channels {
+                display: grid;
+                grid-gap: 15px;
+                padding: 15px;
+                grid-template-columns: repeat(auto-fill, minmax(160px, 1fr))
+            }
+            .channel {
                 display: block;
-                margin: 0 auto;
+                border-radius: 3px;
+                box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+                margin-bottom: 0.5em;
+            }
+            .channel img {
+                width: 100%;
+            }
+            h2 {
+                padding: 5px;
+                font-size: 0.9em;
+                font-weight: 600;
+                margin: 0;
+                text-align: center;
             }
         `}</style>
-      </div>
+
+            <style global jsx>{`
+            body {
+                margin: 0;
+                font-family: system-ui;
+                background: white;
+            }
+        `}</style>
+        </div>
     }
-  }
+}
